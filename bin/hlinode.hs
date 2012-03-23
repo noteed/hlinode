957cbbee84ffe470fb18af4f015d5852fd7189bb
Vo Minh Thu <noteed@gmail.com>
2012-03-23 21:56:41 +0100

    Use cmdargs for the command-line interface.

--- bin/hlinode.hs
+++ bin/hlinode.hs
@@ -1,11 +1,35 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE RecordWildCards #-}
 module Main where
+
+import Data.Version (showVersion)
+import Paths_hlinode (version)
+import System.Console.CmdArgs.Implicit
 
 import Network.Linode
 
 main :: IO ()
-main = do
+main = (processCmd =<<) $ cmdArgs $
+  modes
+    [ cmdDomainList
+    ]
+  &= summary versionString
+  &= program "hlinode"
+
+versionString :: String
+versionString =
+  "hlinode " ++ showVersion version ++ " Copyright (c) 2012 Vo Minh Thu."
+
+data Cmd =
+    CmdDomainList
+  deriving (Data, Typeable)
+
+cmdDomainList :: Cmd
+cmdDomainList = CmdDomainList
+  &= help "List domains the API key have access to."
+
+processCmd :: Cmd -> IO ()
+processCmd CmdDomainList{..} = do
   apiKey <- readFile "api-key.txt"
   domains <- domainList apiKey
   print domains
