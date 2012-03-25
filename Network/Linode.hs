d170edf9419cb7074a060fd06e1114168f8d8ddb
Vo Minh Thu <noteed@gmail.com>
2012-03-25 14:09:40 +0200

    Completed the Domain data type, abstracted the parsing from JSON in the apiCall fuction.

--- Network/Linode.hs
+++ Network/Linode.hs
@@ -22,8 +22,8 @@ apiRequest apiKey parameters = do
 
 -- | Post a request built with `apiRequest` to Linode and return an Aeson
 -- value.
-apiCall :: String -> [(B.ByteString, B.ByteString)] -> IO (Maybe ApiResponse)
-apiCall apiKey parameters = do
+apiCall' :: String -> [(B.ByteString, B.ByteString)] -> IO (Maybe ApiResponse)
+apiCall' apiKey parameters = do
   request <- apiRequest apiKey parameters
   Response{..} <- withManager $ httpLbs request
   case parse json responseBody of
@@ -34,34 +34,63 @@ apiCall apiKey parameters = do
         _ -> return Nothing
     _ -> return Nothing
 
--- | List domains the API key have access to.
-domainList :: String -> IO (Maybe [Domain])
-domainList apiKey = do
-  mApiResponse <- apiCall apiKey [("api_action", "domain.list")]
+-- | Similar to `apiCall'` but extract the `apiResponseData`,
+-- parsed it (using `fromJSON`), and return the result.
+apiCall :: FromJSON a =>
+  String -> [(B.ByteString, B.ByteString)] -> IO (Maybe a)
+apiCall apiKey parameters = do
+  mApiResponse <- apiCall' apiKey parameters
   -- print mApiResponse
   case (fromJSON . apiResponseData) <$> mApiResponse of
-    Just (Success domains) -> return $ Just domains
+    Just (Success x) -> return $ Just x
     x -> do
       -- print x
       return Nothing
 
+-- | List domains the API key have access to.
+domainList :: String -> IO (Maybe [Domain])
+domainList apiKey = apiCall apiKey [("api_action", "domain.list")]
+
 -- | List the resources associated to a domain.
 domainResourceList :: String -> Int -> IO (Maybe ApiResponse)
-domainResourceList apiKey domainId = apiCall apiKey
+domainResourceList apiKey domainId = apiCall' apiKey
   [ ("api_action", "domain.resource.list")
   , ("DomainID", B.pack $ show domainId)
   ]
 
+-- | Represent a domain.
 data Domain = Domain
   { domainId :: Int
+  , domainDescription :: Text
+  , domainType :: Text
+  , domainStatus :: Int
+  , domainSoaEmail :: Text
+  , domainDomain :: Text
+  , domainRetrySec :: Int
+  , domainMasterIps :: Text
+  , domainExpireSec :: Int
+  , domainRefreshSec :: Int
+  , domainTtlSec :: Int
   }
   deriving Show
 
 instance FromJSON Domain where
   parseJSON (Object v) = Domain <$>
-    v .: "DOMAINID"
+    v .: "DOMAINID" <*>
+    v .: "DESCRIPTION" <*>
+    v .: "TYPE" <*>
+    v .: "STATUS" <*>
+    v .: "SOA_EMAIL" <*>
+    v .: "DOMAIN" <*>
+    v .: "RETRY_SEC" <*>
+    v .: "MASTER_IPS" <*>
+    v .: "EXPIRE_SEC" <*>
+    v .: "REFRESH_SEC" <*>
+    v .: "TTL_SEC"
   parseJSON _ = mzero
 
+-- | Represent a Linode response in a slightly more structured data type than
+-- just JSON. Still some parts are kept in JSON.
 data ApiResponse = ApiResponse
   { apiResponseErrors :: [Value]
   , apiResponseAction :: Text
